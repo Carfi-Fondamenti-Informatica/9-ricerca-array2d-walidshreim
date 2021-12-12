7b7ffca7b9b3fe534630abb2d242029f05261b3c
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-12-12 22:48:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+#include <cstring>
 
+bool ricerca (char lista [][], char nome [1][20], int &pos){
+  for(int i=0; i<10; i++){
+    if (strcmp (lista [i], nome [0])==0){
+      pos =i;
+      return true;
+    }
+  }
+  return false;
+}
