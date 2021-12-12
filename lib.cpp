c65e5d62e61e9b644f6c84c958497471fbe3f27d
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-12-12 22:53:40 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 #include "lib.h"
 #include <cstring>
 
-bool ricerca (char lista [][], char nome [1][20], int &pos){
+bool ricerca (char lista [10][20], char nome [1][20], int &pos){
   for(int i=0; i<10; i++){
     if (strcmp (lista [i], nome [0])==0){
       pos =i;
