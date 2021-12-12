9fad0082fc6255d89ee51962b488c843d3d10c40
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-12-12 22:52:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int pos;
+  char a[10][20], f[1][20];
+  for (int i=0; i<1; i++){
+    cin>> a[i];
+  }
+  cin>>f[0];
+  if (ricerca (a, f, pos)){
+    cout<< pos<< endl;
+  }else{
+    cout << "non presente " << endl:
+  }
   return 0;
 }
